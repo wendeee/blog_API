d9358405e2786744b03258fec83a814b57f10e0b
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-11-05 08:41:01 -0700

    update author controller

--- src/api/controllers/author.controller.js
+++ src/api/controllers/author.controller.js
@@ -5,13 +5,11 @@
 const express = require('express');
 const router = express.Router()
 const User = require('../model/User')
+const catchAsyncError = require('../utils/error.catchAsync')
 const Article = require('../model/Article');
+const authorService = require('../services/author.service')
 
-exports.getAllArticles = async(req, res, next) => {
-   
-    
-    
-
+exports.getAllArticles = catchAsyncError(async(req, res, next) => {
     
     const { query } = req;
 
@@ -28,81 +26,9 @@ exports.getAllArticles = async(req, res, next) => {
            res.json('Invalid Page Request')
         }
 
-    let findQuery = {};
-    let articles;
-    try{
-        articles = await Article.find( {state: {$regex:  new RegExp(req.query.state, "i")}, author_Id: req.user._id})
-        res.json(articles)
-    }catch(err){
-        console.log(err)
-    }
-    // {author_Id: req.user._id},
-   
-    // await articles.populate({
-    //     path: '/',
-    //     findQuery
-    // }).execPopulate();
-    // res.status(200).json(req.user.articles)
-
-    // articles = await Article.find({author_Id: req.user._id}).where('state').equals(findQuery.state).skip(skip).limit(limit_per_page)
-    // }
-
-  
-    // if(query.state){
-    //     articles = await Article.find({author_Id: req.user._id}).where('state').equals(findQuery.state).skip(skip).limit(limit_per_page)
-    // }
-    // query = await query
-    // res.json(articles)
-    // await Article.find({author_Id: req.user._id}, query)
-
-   
     
-   
+       const articles = await authorService.getAllArticles( {state: {$regex:  new RegExp(req.query.state, "i")}, author_Id: req.user._id})
+       res.status(200).json(articles)    
+})
     
-
-    // if(!req.query){
-    //     console.log(true)
-    //     articles = await Article.find({author_Id: req.user._id})
-    // }
-    
-        // articles = await Article.find({author_Id: req.user._id}, findQuery).skip(skip).limit(limit_per_page)
-   
-    // articles = await Article.find({author_Id: req.user._id}).where('state').equals(findQuery.state).skip(skip).limit(limit_per_page)
-
-    // console.log(req.user)
-   
-    // articles = await Article.find({author_Id: req.user._id}).where('state').equals(query.state)
-    // res.status(200).json(articles)
-    // console.log(articles)
-   
-
-    // console.log(queryString.state)
-
-    // let query = await Article.find(queryString)
-    // console.log(query)
-    //find user by id
-    // console.log(req.user)
-    // const user = await User.findById(req.params.id);
-
-    // console.log(user)
-    // console.log(user._id)
-    // if(!user) return res.status(404).json('User not found!')
-    // let allPost = await Article.find({$or: [{author_Id: { $eq: req.user._id}},  {state: {$eq: query}}]})
-   
-    // let allPost = await Article.find({$and: [{author_Id: { $eq: user.id}}, {state: {$eq: query}}]}) 
-    
-     
-        // if(!allPost) return res.status(404).json('No post found for this user!')
-        // if(allPost && query){
-        //     // if(query){
-        //       articles = await Article.find({state: query})  
-        //     // }
-        // }
-        // res.json(articles)
-        next()
-     }
-    //  console.log(allPost)
-    // if(!allPost) return res.status(404).json('No post found for this user!')
-
-    // res.json(allPost)
  
