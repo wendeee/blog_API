21795ef814775682b26c7a0d2ac8b23a047695ee
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-11-05 11:24:11 -0700

    update author service

--- src/api/services/author.service.js
+++ src/api/services/author.service.js
@@ -3,7 +3,7 @@ const Author = require('../model/User')
 
 exports.getAllArticles = async(findQuery, skip,limit_per_page) =>{
     try{
-        const articles = await Article.find(findQuery,skip,limit_per_page);
+        const articles = await Article.find(findQuery).skip(skip).limit(limit_per_page);
         return articles;
     }catch(err){
         throw err;
