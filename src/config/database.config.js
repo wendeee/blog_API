96c193e46897cdf7f36021c6a316cd15cd5d4aa7
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-11-04 22:33:07 -0700

    Add config file

---
+++ src/config/database.config.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+const MONGO_DB_CONNECTION_URL= process.env.MONGODB_CONNECTION_URL
+
+const CONFIG = {
+    DB_CONNECTION: function connectDB(){
+        mongoose.connect(MONGO_DB_CONNECTION_URL)
+        mongoose.connection.on('connected', () => {
+            console.log('Connection to DB successful!')
+        })
+
+        mongoose.connection.on('error', (err) => {
+            console.log(err)
+            console.log('Connection to DB failed!')
+        })
+    }
+}
+
+module.exports= CONFIG.DB_CONNECTION
