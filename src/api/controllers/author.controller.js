523f08cd4ce4da52332a600222438e56ce31f757
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-11-05 12:13:51 -0700

    Update author controller

--- src/api/controllers/author.controller.js
+++ src/api/controllers/author.controller.js
@@ -27,7 +27,7 @@ exports.getAllArticles = catchAsyncError(async(req, res, next) => {
         }
 
         let findQuery= req.query.state
-       const articles = await authorService.getAllArticles( {state: {$regex:  new RegExp(findQuery, "i")}, author_Id: req.user._id, limit_per_page,skip})
+       const articles = await authorService.getAllArticles( {state: {$regex:  new RegExp(findQuery, "i")}, author_Id: req.user._id },limit_per_page,skip)
        res.status(200).json(articles)    
 })
     
