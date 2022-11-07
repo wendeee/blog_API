a8306ff238908dcd40c32fed9f5182c9af2af297
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-11-06 19:00:12 -0800

    Refactored code

--- src/api/routes/author.route.js
+++ src/api/routes/author.route.js
@@ -4,4 +4,6 @@ const passport = require('passport');
 const router = express.Router()
 require('../component/authentication/auth')
 router.get('/', passport.authenticate('jwt', {session: false}),authorController.getAllArticles)
+//GET BLOG BY ID
+//DELETE USER ACCOUNT
 module.exports = router;
