d4e1487acda4adbd19a5579cebc61dbd64e55333
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-20 19:43:41 +0100

    improve: update db connection function

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-// const MONGO_DB_CONNECTION_URL= process.env.MONGODB_CONNECTION_URL;
-const MONGO_DB_CONNECTION_URL =  process.env.MONGODB_CONNECTION_URL
+
+
 const  DB_CONNECTION = async()=> {
     try{
-        await mongoose.connect(MONGO_DB_CONNECTION_URL);
+        mongoose.connect(process.env.DATABASE_LOCAL);
         console.log('Connection to DB successful!')
     }catch(err){
         console.log('Connection to DB failed!')
@@ -12,18 +12,3 @@ const  DB_CONNECTION = async()=> {
     
 }
 module.exports = DB_CONNECTION;
-// const CONFIG = {
-//     DB_CONNECTION: function connectDB(){
-//         mongoose.connect(MONGO_DB_CONNECTION_URL)
-//         mongoose.connection.on('connected',() => {
-//             console.log('Connection to DB successful!')
-//         })
-
-//         mongoose.connection.on('error', (err) => {
-//             console.log(err)
-//             console.log('Connection to DB failed!')
-//         })
-//     }
-// }
-
-// module.exports= CONFIG.DB_CONNECTION
