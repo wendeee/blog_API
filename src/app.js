3a30222082b46d4a46725dfb87a5cab3d91c27cb
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-20 19:24:22 +0100

    improve: format code with prettier, import new libraries

--- src/app.js
+++ src/app.js
@@ -1,33 +1,47 @@
-const express = require('express');
-const userAuthRouter = require('./api/routes/UserAuth');
-const articleRouter = require('./api/routes/article.route')
-const authorRouter = require('./api/routes/author.route')
-const globalErrorCatch = require('./api/utils/globalErrorCatch')
+const express = require("express");
+const helmet = require("helmet");
+const { rateLimit } = require("express-rate-limit");
+const userAuthRouter = require("./api/routes/UserAuth");
+const postRouter = require("./api/routes/post.route");
+const authorRouter = require("./api/routes/author.route");
+const globalErrorCatch = require("./api/utils/globalErrorCatch");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
-require('./api/component/authentication/auth')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//configure rate limit obj
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+//security middleware - protect express app
+app.use(helmet());
+
+//implement rate limit
+app.use(limiter);
 
 // ******************MIDDLEWARES*******************//
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use('/', userAuthRouter)
-app.use('/api/v1/blogs', articleRouter)     //general endpoint
-app.use('/api/v1/author/blogs', authorRouter)       //author specific endpoint
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Home Page of the Blog API')
-})
+app.use("/api/auth", userAuthRouter);
+app.use("/api/v1/blogs", postRouter); //general endpoint
+app.use("/api/v1/author/blogs", authorRouter); //author specific endpoint
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Home Page of the Blog API");
+});
 
 //Error handler for unknown route
 app.all("*", (req, res, next) => {
-    const err = new Error(`${req.originalUrl} not found!`);
-    err.status = 'fail',
-    err.StatusCode = 404;
-
-    next(err)
-})
+  const err = new Error(`${req.originalUrl} not found!`);
+  (err.status = "fail"), (err.StatusCode = 404);
+  next(err);
+});
 //Error Handler middleware
-app.use(globalErrorCatch)
+app.use(globalErrorCatch);
 module.exports = app;
