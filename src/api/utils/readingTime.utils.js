8f793c9766ce6a25599d17f72d54a7612d5f5d20
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-20 19:41:44 +0100

    improve: format code with prettier

--- src/api/utils/readingTime.utils.js
+++ src/api/utils/readingTime.utils.js
@@ -1,9 +1,7 @@
-const { req, res } = require('express');
-
 //calculate reading time for an article based on the number of words
-exports.readingTime = (body) => {
-    const wpm = 225;
-    const numOfWords = body.trim().split(/\s+/).length;
-    const readingTime =  Math.ceil(numOfWords / wpm)
-    return readingTime;
-}
+exports.readTime = (body) => {
+  const wpm = 225;
+  const numOfWords = body.trim().split(/\s+/).length;
+  const readingTime = Math.ceil(numOfWords / wpm);
+  return readingTime;
+};
