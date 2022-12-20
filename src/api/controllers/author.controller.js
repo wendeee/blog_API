2cf8e238e9f6874d890db2c4e61d73f495516599
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-20 19:26:27 +0100

    improve: update author's getAllPosts function

--- src/api/controllers/author.controller.js
+++ src/api/controllers/author.controller.js
@@ -1,68 +1,36 @@
 //Authors should be to - Get all blogs they created
 //Blogs end point should be filerable by state
 //Blogs endpoint should be paginated
-//
-const express = require("express");
-const router = express.Router();
-const User = require("../model/User");
+
 const catchAsyncError = require("../utils/error.catchAsync");
-const Article = require("../model/Article");
+
 const authorService = require("../services/author.service");
 
-exports.getAllArticles = catchAsyncError(async (req, res, next) => {
+exports.getAllPosts = catchAsyncError(async (req, res) => {
   const { query } = req;
-
   const {
+    state,
     page = req.query.page * 1 || 1,
-    limit_per_page = req.query.limit * 1 || 10,
-    skip = (page - 1) * limit_per_page,
-    state = req.query.state
+    limit = req.query.limit * 1 || 10,
+    skip = (page - 1) * limit,
   } = query;
+  const filterQuery = {};
 
-  let numOfArticles = await Article.countDocuments();
-  if (skip >= numOfArticles) {
-    res.json("Invalid Page Request");
+  if (state) {
+    filterQuery.state = state;
   }
-const searchQuery = {}
-
-if(state){
-  searchQuery.state = state
-}
-  // const authorBlogs = await User.find({_id: req.user._id, searchQuery}).populate('articles').select('articles')
- 
 
   let authorID = req.user._id;
-  const articleByUser = await User.findById(authorID).populate("articles");
+  const posts = await authorService.getAllPosts(
+    authorID,
+    filterQuery,
+    skip,
+    limit
+  );
 
-  let articles = articleByUser.articles;
-  // const result = await User.find(articles, page).filter()
-
-  let allArticles = articles.filter((article) => {
-    const filters = req.query;
-    for (let key in filters) {
-      articles = article[key] === filters[key];
-    }
-    // if(req.query.page){
-    //   articles = req.query.page
-    // }
-   
-
-    return articles;
-  });
- 
-
-  // console.log(allArticles)
-  // // articles.find(page)
-
-
-
-  //     //  const articles = await authorService.getAllArticles({state: {$regex:  new RegExp(findQuery, "i")}, author_Id: req.user._id },limit_per_page,skip)
   res.status(200).json({
-    // totalBlogs: articles.length,
-    // authorBlogs,
-    allArticles
-    // result
-    // articles,
-    // finalresult
+    page,
+    numOfPosts: posts.length,
+    posts,
   });
 });
