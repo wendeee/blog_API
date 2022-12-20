f27809c731d4888580725f7e734d1495ce8b7cff
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-20 19:39:54 +0100

    improve: update getAllPosts function

---
+++ src/api/services/post.service.js
@@ -0,0 +1,72 @@
+//title, description, tags, author, timestamp, state, read_count, reading_time and body
+const Post = require("../model/Post");
+const User = require('../model/User')
+//get all published Posts
+exports.getAllPosts = async (searchQuery, orderQuery, skip, limit) => {
+  try {
+    const posts = await Post.find({
+      author: { $regex: new RegExp(searchQuery.author, "i") },
+      title: { $regex: new RegExp(searchQuery.title, "i") },
+      tags: { $regex: new RegExp(searchQuery.tags, "i") },
+      state: "published",
+    })
+      .sort(orderQuery)
+      .skip(skip)
+      .limit(limit);
+
+    return posts;
+  } catch (err) {
+    throw err;
+  }
+};
+
+//get a published post by ID
+exports.getPostById = async (id) => {
+  try {
+    const post = await Post.findById({ _id: id })
+      .where("state")
+      .eq("published");
+    post.read_Count === 0 ? post.read_Count++ : post.read_Count++;
+    post.save();
+    return post;
+  } catch (err) {
+    throw err;
+  }
+};
+
+//create a new post
+exports.createPost = async (data) => {
+  try {
+    const post = await Post.create(data);
+    post.save();
+    return post;
+  } catch (err) {
+    throw err;
+  }
+};
+
+//update an existing post
+exports.updatePost = async (data) => {
+  try {
+    const post = await Post.findByIdAndUpdate(data);
+    return post;
+  } catch (err) {
+    throw err;
+  }
+};
+
+//delete an post
+exports.deletePost = async (authorId, postToDelete) => {
+  try {
+    //delete post from user's 'posts' array in user collection
+    const postByUser = await User.findById(authorId);
+    postByUser.posts.pull(postToDelete._id);
+    await postByUser.save();
+
+    //delete post from post collection
+    await Post.deleteOne(postToDelete);
+    return;
+  } catch (err) {
+    throw err;
+  }
+};
