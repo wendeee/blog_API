618f8998897323b1d537c634e87b3187c1ad3446
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2022-12-20 19:37:38 +0100

    improve: add forgotPassword and resetPassword route

--- src/api/routes/UserAuth.js
+++ src/api/routes/UserAuth.js
@@ -1,52 +1,71 @@
-const express = require('express');
-const jwt = require('jsonwebtoken')
-const passport = require('passport')
-require('dotenv').config()
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const userAuth = require("../controllers/user.authController.js");
+require("dotenv").config();
 
-const userAuthRouter = express.Router()
+const userAuthRouter = express.Router();
 
 // ********************SIGN UP USER*********************************//
-userAuthRouter.post('/signup', passport.authenticate('signup', {session:false}), async(req, res, next) => {
+userAuthRouter.post(
+  "/signup",
+  passport.authenticate("signup", { session: false }),
+  async (req, res, next) => {
+    const payload = {
+      _id: req.user._id,
+      email: req.user.email,
+      firstname: req.user.firstname,
+      lastname: req.user.lastname,
+    };
+
+    const token = jwt.sign({ user: payload }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
     res.status(200).json({
-        success: true,
-        message: 'Signed up successfully!'
-        
-    })
-})
+      success: true,
+      message: "Signed up successfully!",
+      token,
+    });
+  }
+);
 
 // *******************************************************************//
 
 // ************************LOGIN USER********************************//
 
-userAuthRouter.post('/login', async(req, res,next) => {
-    passport.authenticate('login', (err,user) => {
-        try{
-            if(err){
-                return next(err)
-            }
-            if(!user){
-                return next(new Error('Username or Password is incorrect'))
-            }
-            req.login(user, {session:false}, async(error) => {
-                if(error) return next(error)
-                const payload = {
-                    _id: user._id, 
-                    email: user.email,
-                    firstname: user.firstname,
-                    lastname: user.lastname
-                }
-                
-                const token = jwt.sign({user: payload}, process.env.JWT_SECRET, { expiresIn: '1h' })
-                res.status(200).json({
-                    success: true,
-                    message: token
-                })
-            })
-        }catch(err){
-            return next(err)
-        }
-        
-    })(req, res, next)
-})
+userAuthRouter.post("/login", async (req, res, next) => {
+  passport.authenticate("login", (err, user) => {
+    try {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return next(new Error("Username or Password is incorrect"));
+      }
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+        const payload = {
+          _id: user._id,
+          email: user.email,
+          firstname: user.firstname,
+          lastname: user.lastname,
+        };
 
+        const token = jwt.sign({ user: payload }, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        res.status(200).json({
+          success: true,
+          token,
+        });
+      });
+    } catch (err) {
+      return next(err);
+    }
+  })(req, res, next);
+});
+//forgot password
+userAuthRouter.post("/forgotPassword", userAuth.forgotPassword);
+userAuthRouter.patch("/resetPassword/:token", userAuth.resetPassword);
 module.exports = userAuthRouter;
