a3590b1637ca40d4d3ed3cf874e08db044d44059
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2023-01-24 22:10:57 +0100

    new: Create a new file for database connection

---
+++ src/db/mongodb.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+const  DB_CONNECTION = async()=> {
+    try{
+        mongoose.connect(process.env.MONGODB_URL);
+        console.log('Connection to DB successful!')
+    }catch(err){
+        console.log('Connection to DB failed!')
+    }
+    
+}
+module.exports = DB_CONNECTION;
