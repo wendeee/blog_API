20c67958a82823e08b3776aa7d2cb3e88c89a8ff
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2023-01-24 22:34:50 +0100

    update: set view engine

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const express = require("express");
+const path = require("path");
 const helmet = require("helmet");
+const cors = require("cors");
 const { rateLimit } = require("express-rate-limit");
+const cookieParser = require("cookie-parser");
+const viewRouter = require("./api/routes/view.routes");
 const userAuthRouter = require("./api/routes/UserAuth");
 const postRouter = require("./api/routes/post.route");
 const authorRouter = require("./api/routes/author.route");
 const globalErrorCatch = require("./api/utils/globalErrorCatch");
 const app = express();
-
-require("dotenv").config();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 //configure rate limit obj
 const limiter = rateLimit({
@@ -20,21 +19,52 @@ const limiter = rateLimit({
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
-//security middleware - protect express app
-app.use(helmet());
-
 //implement rate limit
 app.use(limiter);
 
-// ******************MIDDLEWARES*******************//
+app.use(cors());
 
-app.use("/api/auth", userAuthRouter);
+//security middleware - protect express app
+// app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", "data:", "blob:"],
+      scriptSrc: ["'self'", "https://*.cloudflare.com"],
+      scriptSrcElem: ["'self'", "https:", "https://*.cloudflare.com"],
+      connectSrc: ["'self'", "data", "https://*.cloudflare.com"],
+    },
+  })
+);
+
+//set views and view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/client/views"));
+
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); //parse incoming cookie request
+
+app.use("/public", express.static("src/client/public"));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies)
+  next();
+});
+
+// ****************** ROUTE MIDDLEWARES*******************//
+app.use((req, res, next) => {
+  res.locals.user = null;
+  next();
+});
+app.use("/", viewRouter); //view Endpoint
+app.use("/api/auth", userAuthRouter); //Authentication endpoint
 app.use("/api/v1/blogs", postRouter); //general endpoint
 app.use("/api/v1/author/blogs", authorRouter); //author specific endpoint
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Home Page of the Blog API");
-});
 
 //Error handler for unknown route
 app.all("*", (req, res, next) => {
