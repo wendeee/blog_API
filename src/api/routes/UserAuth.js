2db67a9a143f545c0dd98b6a6f168ad7af04ac07
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2023-01-24 22:38:19 +0100

    update: add cookie and pass to response object

--- src/api/routes/UserAuth.js
+++ src/api/routes/UserAuth.js
@@ -10,7 +10,7 @@ const userAuthRouter = express.Router();
 userAuthRouter.post(
   "/signup",
   passport.authenticate("signup", { session: false }),
-  async (req, res, next) => {
+    async (req, res, next) => {
     const payload = {
       _id: req.user._id,
       email: req.user.email,
@@ -22,11 +22,20 @@ userAuthRouter.post(
       expiresIn: "1h",
     });
 
+//Add Cookie
+    //set Httpcookie
+    res.cookie('jwt', token, {
+      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+      // secure: true,
+      httpOnly: true
+    })
+
     res.status(200).json({
       success: true,
       message: "Signed up successfully!",
       token,
     });
+    next()
   }
 );
 
@@ -41,7 +50,11 @@ userAuthRouter.post("/login", async (req, res, next) => {
         return next(err);
       }
       if (!user) {
-        return next(new Error("Username or Password is incorrect"));
+       return res.status(400).json({
+          status: 'Fail',
+          message: 'Email or Password is incorrect'
+        })
+        // return next(new Error("Username or Password is incorrect"));
       }
       req.login(user, { session: false }, async (error) => {
         if (error) return next(error);
@@ -52,19 +65,33 @@ userAuthRouter.post("/login", async (req, res, next) => {
           lastname: user.lastname,
         };
 
-        const token = jwt.sign({ user: payload }, process.env.JWT_SECRET, {
+        let token = jwt.sign({ user: payload }, process.env.JWT_SECRET, {
           expiresIn: "1h",
         });
+       
+        //set Httpcookie
+        res.cookie('jwt', token, {
+          expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+          // secure: true,
+          httpOnly: true
+        })
+      
+      // console.log(req.cookies.jwt)
+
         res.status(200).json({
-          success: true,
+          status: 'success',
           token,
         });
+        // return res.redirect('/')
       });
     } catch (err) {
-      return next(err);
+      
+      return next(err.message);
     }
   })(req, res, next);
 });
+
+userAuthRouter.get('/logout', userAuth.logout)
 //forgot password
 userAuthRouter.post("/forgotPassword", userAuth.forgotPassword);
 userAuthRouter.patch("/resetPassword/:token", userAuth.resetPassword);
