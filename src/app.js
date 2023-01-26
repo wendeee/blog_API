8be657c7a91c3f279a45f1fc84b8248862787002
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2023-01-26 05:07:16 +0100

    fix: update request time in rateLimit

--- src/app.js
+++ src/app.js
@@ -14,8 +14,8 @@ const app = express();
 
 //configure rate limit obj
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 25 * 60 * 1000, // 25 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 25 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
